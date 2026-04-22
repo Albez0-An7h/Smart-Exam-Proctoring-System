f295dfa6afca0b21019bb6bd223f058e3fe14408
Albez0-An7h <ansh.km7096@gmail.com>
2026-04-22 23:04:22 +0530

    fix: update prisma build command and remove unused prisma/config dependency and file

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,11 +1,1 @@
-import { defineConfig } from 'prisma/config'
-
-export default defineConfig({
-  schema: 'prisma/schema.prisma',
-  migrations: {
-    path: 'prisma/migrations',
-  },
-  datasource: {
-    url: process.env.DATABASE_URL!,
-  },
-})
+export {}
